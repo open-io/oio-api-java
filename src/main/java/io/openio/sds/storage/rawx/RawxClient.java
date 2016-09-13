73acff59a418756e30b8726d2ea9f4fc7e815ed4
Florent Vennetier (OpenIO) <florent@fridu.net>
2016-09-13 18:44:02 +0200

    RawxClient: add retry loop around upload task submission

--- src/main/java/io/openio/sds/storage/rawx/RawxClient.java
+++ src/main/java/io/openio/sds/storage/rawx/RawxClient.java
@@ -31,6 +31,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -223,91 +224,108 @@ public class RawxClient implements StorageClient {
 
 	/* --- INTERNALS --- */
 
-	private ObjectInfo uploadPosition(final ObjectInfo oinf,
-	        final int pos, final Long size, InputStream data,
-	        final String reqId) {
-		List<ChunkInfo> cil = oinf.sortedChunks().get(pos);
-		final List<FeedableInputStream> gens = size == 0 ? null
-		        : feedableBodys(cil.size(), size);
-		List<Future<OioException>> futures = new ArrayList<Future<OioException>>();
-		for (int i = 0; i < cil.size(); i++) {
-			final ChunkInfo ci = cil.get(i);
-			final FeedableInputStream in = null == gens ? null : gens.get(i);
+    private ObjectInfo uploadPosition(final ObjectInfo oinf, final int pos, final Long size,
+            InputStream data, final String reqId) {
+        List<ChunkInfo> cil = oinf.sortedChunks().get(pos);
+        final List<FeedableInputStream> gens = size == 0 ? null : feedableBodys(cil.size(), size);
+        List<Future<OioException>> futures = new ArrayList<Future<OioException>>();
+        for (int i = 0; i < cil.size(); i++) {
+            final ChunkInfo ci = cil.get(i);
+            final FeedableInputStream in = null == gens ? null : gens.get(i);
 
-			futures.add(executors.submit(new Callable<OioException>() {
+            Callable<OioException> uploader = new Callable<OioException>() {
 
-				@Override
-				public OioException call() {
-					try {
-						RequestBuilder builder = http.put(ci.url())
-			                    .header(CHUNK_META_CONTAINER_ID,
-			                            oinf.url().cid())
-			                    .header(CHUNK_META_CONTENT_ID, oinf.oid())
-			                    .header(CHUNK_META_CONTENT_VERSION,
-			                            String.valueOf(oinf.version()))
-			                    .header(CHUNK_META_CONTENT_POLICY,
-			                            oinf.policy())
-			                    .header(CHUNK_META_CONTENT_MIME_TYPE,
-			                            oinf.mtype())
-			                    .header(CHUNK_META_CONTENT_CHUNK_METHOD,
-			                            oinf.chunkMethod())
-			                    .header(CHUNK_META_CONTENT_CHUNKSNB,
-			                            String.valueOf(oinf.nbchunks()))
-			                    .header(CHUNK_META_CONTENT_SIZE,
-			                            String.valueOf(oinf.size()))
-			                    .header(CHUNK_META_CONTENT_PATH,
-			                            oinf.url().object())
-			                    .header(CHUNK_META_CHUNK_ID, ci.id())
-			                    .header(CHUNK_META_CHUNK_POS,
-			                            ci.pos().toString())
-			                    .header(OIO_REQUEST_ID_HEADER, reqId)
-			                    .verifier(RAWX_VERIFIER);
-						if (null == gens)
-							builder.body("");
-						else
-							builder.body(in, size);
-						ci.size(size);
-						ci.hash(builder.execute()
-			                    .close(false)
-			                    .header(CHUNK_META_CHUNK_HASH));
-					} catch (OioException e) {
-						return e;
-					}
-					return null;
-				}
-			}));
-		}
-		consume(data, size, gens, futures);
-		try {
-			ArrayList<OioException> failures = new ArrayList<OioException>();
-			for (Future<OioException> f : futures) {
-				OioException e;
-				try {
-					e = f.get(60, TimeUnit.SECONDS);
-				} catch (TimeoutException e1) {
-					e = new OioException("Chunk upload timeout", e1);
-				}
-				// TODO: log the URL of the chunk that failed
-				if (null != e) {
-					logger.warn("Failed to upload chunk", e);
-					failures.add(e);
-				}
-			}
-			if (failures.size() >= cil.size()) {
-				throw new OioException("All chunk uploads failed",
-				        failures.get(0));
-			} else if (failures.size() > 0) {
-				logger.warn(String
-				        .format("%1$d of %2$d uploads failed for position %3$d of %4$s",
-				                failures.size(), cil.size(), pos, oinf.url()));
-			}
-		} catch (InterruptedException e) {
-			throw new OioException("got interrupted", e);
-		} catch (ExecutionException e) {
-			throw new OioException("Execution exception", e.getCause());
-		}
-		return oinf;
-	}
+                @Override
+                public OioException call() {
+                    try {
+                        RequestBuilder builder = http
+                                .put(ci.url())
+                                .header(CHUNK_META_CONTAINER_ID, oinf.url().cid())
+                                .header(CHUNK_META_CONTENT_ID, oinf.oid())
+                                .header(CHUNK_META_CONTENT_VERSION, String.valueOf(oinf.version()))
+                                .header(CHUNK_META_CONTENT_POLICY, oinf.policy())
+                                .header(CHUNK_META_CONTENT_MIME_TYPE, oinf.mtype())
+                                .header(CHUNK_META_CONTENT_CHUNK_METHOD, oinf.chunkMethod())
+                                .header(CHUNK_META_CONTENT_CHUNKSNB,
+                                        String.valueOf(oinf.nbchunks()))
+                                .header(CHUNK_META_CONTENT_SIZE, String.valueOf(oinf.size()))
+                                .header(CHUNK_META_CONTENT_PATH, oinf.url().object())
+                                .header(CHUNK_META_CHUNK_ID, ci.id())
+                                .header(CHUNK_META_CHUNK_POS, ci.pos().toString())
+                                .header(OIO_REQUEST_ID_HEADER, reqId).verifier(RAWX_VERIFIER);
+                        if (null == gens)
+                            builder.body("");
+                        else
+                            builder.body(in, size);
+                        ci.size(size);
+                        ci.hash(builder.execute().close(false).header(CHUNK_META_CHUNK_HASH));
+                    } catch (OioException e) {
+                        return e;
+                    }
+                    return null;
+                }
+            };
+            int retry = 0;
+            try {
+                while (true) {
+                    try {
+                        Future<OioException> upload = executors.submit(uploader);
+                        futures.add(upload);
+                        break;
+                    } catch (RejectedExecutionException ree) {
+                        if (retry < 5) {
+                            int delay = 1 << retry;
+                            logger.warn("Failed to start rawx upload, retry in " + delay + "s",
+                                    ree);
+                            try {
+                                Thread.sleep(delay * 1000);
+                            } catch (InterruptedException e) {
+                                throw new OioException("Failed to retry rawx upload", e);
+                            }
+                        } else {
+                            throw new OioException("Failed to schedule rawx upload", ree);
+                        }
+                        retry++;
+                    }
+                }
+            } catch (RuntimeException e) {
+                try {
+                    in.close();
+                } catch (IOException e1) {
+                    logger.warn(e1);
+                }
+                throw e;
+            }
+        }
+        consume(data, size, gens, futures);
+        try {
+            ArrayList<OioException> failures = new ArrayList<OioException>();
+            for (Future<OioException> f : futures) {
+                OioException e;
+                try {
+                    e = f.get(60, TimeUnit.SECONDS);
+                } catch (TimeoutException e1) {
+                    e = new OioException("Chunk upload timeout", e1);
+                }
+                // TODO: log the URL of the chunk that failed
+                if (null != e) {
+                    logger.warn("Failed to upload chunk", e);
+                    failures.add(e);
+                }
+            }
+            if (failures.size() >= cil.size()) {
+                throw new OioException("All chunk uploads failed", failures.get(0));
+            } else if (failures.size() > 0) {
+                logger.warn(String.format("%1$d of %2$d uploads failed for position %3$d of %4$s",
+                        failures.size(), cil.size(), pos, oinf.url()));
+            }
+        } catch (InterruptedException e) {
+            throw new OioException("got interrupted", e);
+        } catch (ExecutionException e) {
+            throw new OioException("Execution exception", e.getCause());
+        }
+        return oinf;
+    }
 
 	private void consume(InputStream data, Long size,
 	        List<FeedableInputStream> gens,
